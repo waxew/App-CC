f1b0e22020a80bab500b80b0700a599e571301a7
waxew <4622.ar@gmail.com>
2026-08-26 14:14:28 +0330

    fix: import Compose Image in information screens

--- app/src/main/java/com/asteam/cactuscollection/ScreensInfo.kt
+++ app/src/main/java/com/asteam/cactuscollection/ScreensInfo.kt
@@ -3,6 +3,7 @@ package com.asteam.cactuscollection
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
